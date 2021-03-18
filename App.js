ea1da828fde424afe8a315e59f8be3b1fbb0a202
Bruno Sousa <brse01@gmail.com>
2021-03-18 20:39:30 -0300

    chore: add npm

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {StatusBar} from 'react-native';
+
+const App = () => {
+  return <StatusBar barStyle="light-content" backgroundColor="#151515" />;
+};
+
+export default App;
